331b66bc54206d00d2600c64d2463622c27bd738
Jonathan <lennysgarage1@gmail.com>
2016-12-26 22:36:52 -0500

    Added Discord bot

---
+++ SomethingRandom.java
@@ -0,0 +1,46 @@
+/**
+ * Created by jonathanmarcantonio on 2016-12-26.
+ */
+
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.Channel;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageChannel;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class SomethingRandom extends ListenerAdapter {
+
+    private static JDA jda;
+
+    public static void main(String [] args) {
+        boolean running = true;
+        try {
+            jda = new JDABuilder(AccountType.BOT).setToken("MjYzMTM2NjcyODQ4MjgxNjAy.C0Norg.Ad6tXXUvu0khcnSUmWgeOIOc4A8").addListener(new SomethingRandom()).buildBlocking();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+        @Override
+        public void onMessageReceived(MessageReceivedEvent event) {
+            JDA jda = event.getJDA();
+            Message message = event.getMessage();
+            MessageChannel channel = event.getChannel();
+
+            String msg = message.getContent();
+            if (event.isFromType(ChannelType.TEXT)) {
+                if (msg.equalsIgnoreCase("~test")) {
+                    System.out.println("Working here too!");
+                    channel.sendMessage("Working").queue();
+                }
+                else if(msg.equalsIgnoreCase("~help")){
+                    channel.sendMessage("Help:").queue();
+                    channel.sendMessage("~help - List of commands").queue();
+                    channel.sendMessage("~test - Runs a test").queue();
+                }
+            }
+        }
+}
