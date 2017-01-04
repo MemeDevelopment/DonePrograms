1575026ee2c5b261b5faebbd32587cc78bca51c1
Jonathan <lennysgarage1@gmail.com>
2017-01-04 01:04:51 -0500

    Made it smaller and faster no longer needed to convert a string to char

--- PasswordGen.java
+++ PasswordGen.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 
 public class PasswordGen {
     public static void main(String [] args){
-        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890~@#$%^&*(){}";//Need it in string so its easier than writing {"",""}
-        char[] passWdOptions = alphabet.toCharArray();//Converting the string to char to loop for a rand character.
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxy1234567890~@#$%^&*(){}".toCharArray();//Easier to convert then writing each individual
         String password = "";
         Scanner in = new Scanner(System.in);
         System.out.println("Enter length of password");
         int length = in.nextInt();
         for(int i = 0; i < length; i++){
-            int rand = (int)(Math.random() *  passWdOptions.length + 1);
-            password = password + passWdOptions[rand];
+            int rand = (int)(Math.random() *  alphabet.length + 1);
+            password = password + alphabet[rand];
         }
         System.out.println("New password:\n\n" + password);
     }
