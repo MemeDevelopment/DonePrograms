2a03d1ea7f41adb08e9d66737458ee950798c2f9
lennysgarage <lennysgarage1@gmail.com>
2017-01-06 19:36:13 -0500

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -7,13 +7,11 @@ public class ReverseString {
     public static void main(String [] args){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter a sentence to be reversed!");
-        String sentence = in.nextLine();
+        String sentence = in.nextLine(),temp = "";
         char[] reverse = sentence.toCharArray();
-        String temp = "";
         for(int i = reverse.length-1; i >= 0; i--) {//Sets i to length of char - 1, so last letter is written first.
              temp = temp + reverse[i];
         }
         System.out.println("Reversed string is: " + temp);
     }
-
 }
