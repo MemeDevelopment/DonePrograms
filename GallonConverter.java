a43c12fee024c0d948f61dd8e4a82ff9ae0e48e3
Jonathan <lennysgarage1@gmail.com>
2017-01-08 19:25:43 -0500

    Gallon - Litre converter & vice-versa

---
+++ GallonConverter.java
@@ -0,0 +1,37 @@
+/**
+ * Created by jonathanmarcantonio on 2017-01-08.
+ */
+import java.util.Scanner;
+
+public class GallonConverter {
+    public static void main(String [] args){
+        Scanner in = new Scanner(System.in);
+        boolean running = true;
+        while(running) {
+            System.out.println("Convert Gallons to litres? (N/Y/EXIT)");
+            String question = in.next();
+            if (question.equalsIgnoreCase("N")) {
+                System.out.println("Converting Litres to gallons then!");
+                System.out.println("Insert amount of Litres to be converted!");
+                double amount = in.nextDouble();
+                GallonConverter.convertLitres(amount);
+            } else if (question.equalsIgnoreCase("Y")) {
+                System.out.println("Insert amount of Gallons to be converted!");
+                double amount = in.nextDouble();
+                GallonConverter.convertGallons(amount);
+            }
+            else if (question.equalsIgnoreCase("EXIT")){
+                running = false;
+                System.out.println("Goodbye ;(");
+            }
+        }
+    }
+    public static void convertGallons(double Gallons){
+        double litre = Gallons*3.78541;
+        System.out.printf("%.2f Gallons is equal to %.5f Litres.\n",Gallons,litre);
+    }
+    public static void convertLitres(double Litres){
+        double gallons = Litres*0.264172;
+        System.out.printf("%.2f Litres is equal to %.5f Gallons.\n",Litres,gallons);
+    }
+}
